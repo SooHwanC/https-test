fe8ed7fbed486192395804fe1db3b791eb55eb3b
최수환 <frcp9408@gmail.com>
2023-10-14 16:34:08 +0900

    asdf

--- src/App3.js
+++ src/App3.js
@@ -1,32 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function App() {
-    const getScreenStream = (callback) => {
-        if (navigator.mediaDevices.getDisplayMedia) {
-            navigator.mediaDevices.getDisplayMedia({ video: true })
-                .then(screenStream => {
-                    callback(screenStream);
-                })
-                .catch(error => {
-                    console.error('Error accessing screen stream:', error);
-                });
-        } else {
-            alert('Your browser does not support screen sharing');
-        }
-    };
+    const videoRef = useRef();
 
-    const handleGetScreen = () => {
-        getScreenStream(screenStream => {
-            const video = document.querySelector('video');
-            video.srcObject = screenStream;
-        });
-    };
+    useEffect(() => {
+        const startScreenShare = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getDisplayMedia({ video: true });
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                }
+            } catch (error) {
+                console.error('Error accessing screen stream:', error);
+            }
+        };
+
+        startScreenShare();
+
+        return () => {
+            const stream = videoRef.current.srcObject;
+            if (stream) {
+                const tracks = stream.getTracks();
+                tracks.forEach(track => track.stop());
+            }
+        };
+    }, []);
 
     return (
         <div>
             <h1>화면공유 테스트</h1>
-            <button onClick={handleGetScreen}>Get Screen</button>
-            <video autoPlay controls />
+            <video ref={videoRef} autoPlay controls />
         </div>
     );
 }
