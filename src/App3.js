92b0b49ec99370511b50d3723f7379594d15907f
최수환 <frcp9408@gmail.com>
2023-10-16 10:43:46 +0900

    asd

--- src/App3.js
+++ src/App3.js
@@ -1,35 +1,40 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
-    const videoRef = useRef();
+    const getScreenStream = (callback) => {
+        if (navigator.getDisplayMedia) {
+            navigator.getDisplayMedia({ video: true })
+                .then(screenStream => {
+                    callback(screenStream);
+                })
+                .catch(error => {
+                    console.error('Error accessing screen stream:', error);
+                });
+        } else if (navigator.mediaDevices.getDisplayMedia) {
+            navigator.mediaDevices.getDisplayMedia({ video: true })
+                .then(screenStream => {
+                    callback(screenStream);
+                })
+                .catch(error => {
+                    console.error('Error accessing screen stream:', error);
+                });
+        } else {
+            alert('Your browser does not support screen sharing');
+        }
+    };
 
-    useEffect(() => {
-        const startScreenShare = async () => {
-            try {
-                const stream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-                if (videoRef.current) {
-                    videoRef.current.srcObject = stream;
-                }
-            } catch (error) {
-                console.error('Error accessing screen stream:', error);
-            }
-        };
-
-        startScreenShare();
-
-        return () => {
-            const stream = videoRef.current.srcObject;
-            if (stream) {
-                const tracks = stream.getTracks();
-                tracks.forEach(track => track.stop());
-            }
-        };
-    }, []);
+    const handleGetScreen = () => {
+        getScreenStream(screenStream => {
+            const video = document.querySelector('video');
+            video.srcObject = screenStream;
+        });
+    };
 
     return (
         <div>
             <h1>화면공유 테스트</h1>
-            <video ref={videoRef} autoPlay controls />
+            <button onClick={handleGetScreen}>Get Screen</button>
+            <video autoPlay controls />
         </div>
     );
 }
