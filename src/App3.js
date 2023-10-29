ac406c9b6fe0a621d77d51ace334e10dca36bc47
SooHwanC <frcp9408@gmail.com>
2023-10-30 08:53:54 +0900

    init

--- src/App3.js
+++ src/App3.js
@@ -1,6 +1,30 @@
 import React, { useEffect } from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket'; // websocket 패키지 불러오기
+import axios from 'axios';
 
 function App() {
+
+    // const socket = new W3CWebSocket('ws://localhost:8085/CodeBridge/websocket');
+
+    // socket.onopen = () => {
+    //     console.log('WebSocket 연결 성공');
+    // };
+
+    // socket.onmessage = (event) => {
+    //     const message = event.data;
+    //     console.log('받은 메시지:', message);
+    // };
+
+    // axios.get('http://localhost:8085/CodeBridge/your-endpoint')
+    //     .then(response => {
+    //         console.log('성공?', response.data);
+    //     })
+    //     .catch(error => {
+    //         console.error(error);
+    //     });
+
+
+
     const getScreenStream = (callback) => {
         if (navigator.getDisplayMedia) {
             navigator.getDisplayMedia({ video: true })
